74de388f0cc733429c4f8362524a246a4e39ef9e
shreysharma629 <sharmashrey629@gmail.com>
2025-09-21 20:03:09 +0530

    Q4

---
+++ EXP-2/PrimeCheck.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Shrey Sharma 24csu193");
+        System.out.print("enter a number: ");
+        int n = scan.nextInt();
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
