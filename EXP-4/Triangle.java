7415eee3c70e5cbb644e167d8aebe5f552e9610a
shreysharma629 <sharmashrey629@gmail.com>
2025-09-21 23:08:55 +0530

    Q2

---
+++ EXP-4/Triangle.java
@@ -0,0 +1,37 @@
+class Triangle {
+    private int a, b, c;
+
+    public Triangle() {
+        this.a = 3;
+        this.b = 4;
+        this.c = 5;
+    }
+
+    public int getA() { return a; }
+    public int getB() { return b; }
+    public int getC() { return c; }
+
+    public int getPerimeter() {
+        return a + b + c;
+    }
+
+    public double getArea() {
+        double s = getPerimeter() / 2.0;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public void display() {
+        System.out.println("Sides of Triangle: " + a + ", " + b + ", " + c);
+        System.out.println("Perimeter: " + getPerimeter());
+        System.out.printf("Area: %.2f%n", getArea());
+    }
+}
+
+class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Shrey Sharma 24csu193");
+
+        Triangle t1 = new Triangle();
+        t1.display();
+    }
+}
