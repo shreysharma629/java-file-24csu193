b8e538a19786f0e079b73be622fbd26a4b3c5599
shreysharma629 <sharmashrey629@gmail.com>
2025-11-16 23:20:45 +0530

    Q1

---
+++ EXP 7/PackageDemo.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class PackageDemo {
+    public void display() {
+        System.out.println("PackageDemo executed");
+        System.out.println("shrey sharma 24csu193");
+    }
+}
